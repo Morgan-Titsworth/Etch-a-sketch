1979d952fa1eff4d6b5f33c9cd3daac97fed8d1e
Titsworth <morgan@tobedetermined.dev>
2022-06-12 13:26:09 -0400

    final

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let color = 'black';
+let click = true;
 
 function populateBoard(size){
     let board = document.querySelector(".board");
@@ -20,20 +21,40 @@ populateBoard(16);
 
 function changeSize(input) {
     if(input >= 4 && input <= 100){
+        document.querySelector('.error').style.display = 'none';
         populateBoard(input);
     } else {
-        console.log("pick new number");
+        document.querySelector('.error').style.display = 'flex';
     }
 }
 
 function colorSquare(){
-    if((color = 'random')){
-        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
-    } else {
-        this.style.backgroundColor = color;
+    if(click){
+        if(color === 'random'){
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+        } else {
+            this.style.backgroundColor = color;
+        }
     }
+
 }
 
 function changeColor(choice){
     color = choice;
 }
+
+function resetBoard(){
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => div.style.backgroundColor = 'white');
+}
+document.querySelector('body').addEventListener('click', (e) => {
+   if(e.target.tagName != 'BUTTON'){
+    click = !click;
+    if(click){
+        document.querySelector('.mode').textContent = "Mode: Coloring"
+    } else {
+        document.querySelector('.mode').textContent = "Mode: Not Coloring"
+    }
+   }
+})
