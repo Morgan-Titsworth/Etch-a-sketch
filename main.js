1d418e7b028fd6159898b9074995db7020bf5fc8
Titsworth <morgan@tobedetermined.dev>
2022-06-12 11:07:42 -0400

    paused here

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let color = 'black';
+
 function populateBoard(size){
     let board = document.querySelector(".board");
     let squares = board.querySelectorAll("div");
@@ -8,7 +10,8 @@ function populateBoard(size){
     let amount = size * size;
     for(let i = 0; i < amount; i++) {
         let square = document.createElement("div");
-        square.style.backgroundColor = "blue";
+        square.addEventListener("mouseover", colorSquare);
+        square.style.backgroundColor = "white";
         board.insertAdjacentElement("beforeend", square);
     }
 }
@@ -21,4 +24,16 @@ function changeSize(input) {
     } else {
         console.log("pick new number");
     }
+}
+
+function colorSquare(){
+    if((color = 'random')){
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+    } else {
+        this.style.backgroundColor = color;
+    }
+}
+
+function changeColor(choice){
+    color = choice;
 }
